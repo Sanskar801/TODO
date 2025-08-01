820004d63b3f77acbeccedb3ad74c11011aa5184
Sanskar Jain <sanskarj257@gmail.com>
2025-08-01 22:51:24 +0530

    Focus Session Structure

--- src/component/FocusSession.jsx
+++ src/component/FocusSession.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
 
 const FocusSession = () => {
-  return (
-    <div>FocusSession</div>
-  )
+
+    const [timer, setTimer] = useState('');
+
+    return (
+        <div className='focus-session'>
+
+            <h3>Focus Session</h3>
+
+            <input
+                type="time"
+                name="timer"
+                id="timer"
+                value={timer}
+                onChange={(e) => setTimer(e.target.value)} />
+
+            <button>Start</button>
+
+        </div>
+    )
 }
 
 export default FocusSession
